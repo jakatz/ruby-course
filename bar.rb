98728f78f6f0d030c0a1bfd9d6802b2d51f1221c
Jonathan Katz <jonathan.a.katz@gmail.com>
2014-06-05 23:02:59 -0700

    created setter method for Bar class, and first 9 tests pass

--- bar.rb
+++ bar.rb
@@ -4,16 +4,36 @@ class Bar
   attr_reader :name, :menu_items
   attr_accessor :happy_discount
 
-  def initialize(name, menu_items = [])
+  def initialize(name, menu_items = [], happy_discount = 0)
     @name = name
     @menu_items = menu_items
-    @happy_discount = 0
+    @happy_discount = happy_discount
   end
 
   def add_menu_item(name, price)
     @menu_items << (MenuItem.new(name, price))
   end
 
+  def happy_discount
+    if self.happy_hour? == true
+      @happy_discount
+    else
+      return 0
+    end
+  end
+
+  def happy_discount=(value)
+    if value < 0
+      @happy_discount = 0
+    elsif value > 1
+      @happy_discount = 1
+    else
+      @happy_discount = value
+    end
+  end
+
+  def happy_hour?
+  end
 
 end
 
